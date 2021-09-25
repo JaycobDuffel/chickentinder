bdabdf6c7270ff28031e3ccb16a2ba0193b02b12
Jaycob <jaycobduffel@gmail.com>
2021-09-24 20:43:35 -0700

    Refactor topnav to own component

--- src/Navigation/AppNavigator.js
+++ src/Navigation/AppNavigator.js
@@ -1,33 +1,8 @@
-import React, { useContext } from 'react';
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-
-import HomeScreen from '../Screens/HomeScreen';
-import AccountScreen from '../Screens/AccountScreen';
-
-const Tab = createBottomTabNavigator();
+import React from 'react';
+import TopTabNavigator from './TopTabNavigator';
 
 export default function AppNavigator() {
   return (
-      <Tab.Navigator screenOptions={{tabBarActiveTintColor: 'orange', headerShown: false}}>
-      <Tab.Screen
-        name="Home"
-        component={HomeScreen}
-        options={{
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="home" color={color} size={size} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Account"
-        component={AccountScreen}
-        options={{
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="account" color={color} size={size} />
-          ),
-        }}
-      />
-    </Tab.Navigator>
+      <TopTabNavigator /> 
   );
 };
